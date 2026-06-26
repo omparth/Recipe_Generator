062bb92f72da9a7d8385b591ec42e500ed749a85
Omparth Chougale <147957714+omparth@users.noreply.github.com>
2026-06-26 09:43:17 +0530

    Update gemini.ts

--- src/services/gemini.ts
+++ src/services/gemini.ts
@@ -1,4 +1,3 @@
-//src/services/gemini.tsx
 import { Recipe } from '../types/recipe';
 import {
   GEMINI_SYSTEM_PROMPT,
@@ -9,12 +8,10 @@ console.log("gemini.ts loaded");
 
 const OPENROUTER_API_URL=
 'https://openrouter.ai/api/v1/chat/completions';
-// In Expo, use EXPO_PUBLIC_ prefix to make variables available in the client
 
 declare const __DEV__: boolean;
 
 export const generateRecipe = async (ingredients: string): Promise<Recipe> => {
-  // @ts-ignore - EXPO_PUBLIC variables are injected by Expo at build time
   const apiKey = process.env.EXPO_PUBLIC_OPENROUTER_API_KEY;
     console.log("API KEY FOUND:", !!apiKey);
     console.log("FULL KEY:", apiKey);
@@ -27,11 +24,8 @@ console.log("INGREDIENTS:", ingredients);
     const prompt = RECIPE_PROMPT_TEMPLATE(ingredients);
 
     console.log("Calling OpenRouter API...");
-    // const controller = new AbortController();
 
-    // setTimeout(() => {
-    //   controller.abort();
-    // }, 10000);
+   
     
     const response = await fetch(
       OPENROUTER_API_URL ,
@@ -68,14 +62,12 @@ console.log("INGREDIENTS:", ingredients);
     console.log("openrouter Response:");
 console.log(JSON.stringify(data, null, 2));
 
-    // Extract the text response from Gemini
     const textContent =
     data.choices?.[0]?.message?.content;
     if (!textContent) {
       throw new Error('No content received from Gemini API');
     }
 
-    // Parse and validate the recipe
     const recipe = parseRecipeResponse(textContent);
 
     return recipe;
